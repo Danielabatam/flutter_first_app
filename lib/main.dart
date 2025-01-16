734cba1ebab796cd43f5d079da443d884806c018
Daniela BATAM <danielabatam@gmail.com>
2025-01-16 17:25:18 +0100

    Building custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  Colors.deepPurple,
-                  Colors.deepPurpleAccent
-                ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child:  Center(
-              child: Text('Hello world !',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,))
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.deepPurpleAccent],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+          child: Text('Hello world !',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ))),
+    );
+  }
 }

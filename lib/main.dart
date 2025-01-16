0b49d089babecc5125023626479195c9b5e9a36b
Daniela BATAM <danielabatam@gmail.com>
2025-01-16 10:02:42 +0100

    Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // runApp(const MaterialApp(home: Text('Hello world !') ));
-  // runApp(const MaterialApp(home: Scaffold(body: Text('Hello world !'))));
   runApp(
-    const MaterialApp(
+     MaterialApp(
       home: Scaffold(
-        // backgroundColor: Colors.purple,
-        // backgroundColor: Color.fromARGB(255, 63, 5, 120),
-        body: Center(
-          child: Text('Hello world !'),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Colors.deepPurple,
+                  Colors.deepPurpleAccent
+                ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello world !'),
+          ),
         ),
       ),
     ),
